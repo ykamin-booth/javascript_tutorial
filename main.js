0dc59f86cedaa690decf3be58792edbca4a4e2f5
Ubuntu <ubuntu@ip-172-31-38-235.us-east-2.compute.internal>
2020-08-14 06:12:44 +0000

    updated

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 let Phrase = require("skamin-palindrome");
 
-let string = prompt("Please enter a string for palindrome testing:");
-
-let phrase = new Phrase(string)
-
-if (phrase.palindrome()) {
-  alert(`"${phrase.content}" is a palindrome!`);
-} else {
-  alert(`"${phrase.content}" is not a palindrome.`);
+function palindromeTester(event) {
+  event.preventDefault()
+  let phrase = new Phrase(event.target.phrase.value);
+  let palindromeResult = document.querySelector("#palindromeResult");
+  
+  if (phrase.palindrome()) {
+    palindromeResult.innerHTML = `<strong>"${phrase.content}"</strong> is a palindrome!`;
+  } else {
+    palindromeResult.innerHTML = `<strong>"${phrase.content}"</strong> is not a palindrome.`;
+  }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+let form = document.querySelector("#palindromeTester");
+form.addEventListener("submit", function(event) {
+  palindromeTester(event);
+});
+});
--- palindrome.js
+++ palindrome.js
@@ -1,8 +1,10 @@
+module.exports = Phrase;
 // Adds `reverse` to all strings.
 String.prototype.reverse = function() {
   return Array.from(this).reverse().join("");
 }
 
+//exercises
 String.prototype.blank = function() {
   return !!(this.match(/^\s*$/g));
 }
@@ -34,12 +36,22 @@ function Phrase(content) {
   
   // Returns content processed for palindrome testing.
   this.processedContent = function processedContent() {
-    return this.processor(this.content);
+    return this.processor(this.letters());
+  }
+  
+  // Returns the letters in the content.
+  this.letters = function letters() {
+    const letterRegex = /[a-z]/gi;
+    return (this.content.match(/[a-z]/gi) || []).join("");
   }
   
   // Returns true if the phrase is a palindrome, false otherwise.
   this.palindrome = function palindrome() {
+    if (this.processedContent()) {
     return this.processedContent() === this.processedContent().reverse();
+  } else {
+    return false;
+    }
   }
 }
 
